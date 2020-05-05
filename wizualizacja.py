e9977a9238dc702e84e235bb7a626ab372b6c5c2
krzychu9613 <krzychu960713@gmail.com>
2020-05-05 18:18:35 +0200

    graf + ścieżka

--- wizualizacja.py
+++ wizualizacja.py
@@ -1,12 +1,14 @@
 import networkx as nx
 import numpy as np
+from networkx.drawing.nx_agraph import write_dot, graphviz_layout, to_agraph
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import pylab
 
 
-Robot = nx.OrderedDiGraph()
-Ladowanie = nx.OrderedDiGraph()
-Przybijanie = nx.OrderedDiGraph()
+Robot = nx.MultiDiGraph()
+Ladowanie = nx.MultiDiGraph()
+Przybijanie = nx.MultiDiGraph()
 s1 = "Wozek\nwysuniety"
 s2 = "Wozek\nwsuniety"
 s3 = "Robot\ngotowy"
@@ -22,27 +24,50 @@ s12 = "Przybij\nklocek"
 s13 = "Pozycja\narsenal"
 s14 = "Laduj\nzszywki"
 s15 = "Alarm\nzszywki"
-Robot.add_edges_from([(s1, s2), (s2, s1), (s2, s3), (s3, s2), (s3, s4), (s4,s3), (s3, s5), (s5,s3)])
-Przybijanie.add_edges_from([(s5,s6),(s6,s7),(s6,s8),(s7,s9),(s9,s7),(s8,s10),(s10,s8),(s7,s11),(s8,s11),(s11,s12),(s12,s6)])
-Ladowanie.add_edges_from([(s4,s13),(s13,s15),(s15,s13),(s13,s14),(s14,s13)])
-# Robot.add_edges_from(Przybijanie.edges)
-# Robot.add_edges_from(Ladowanie.edges)
-#
-#
-# val_map = {'A': 1.0, 'D': 0.5714285714285714, 'H': 0.0}
-#
-# values = [val_map.get(node, 0.45) for node in G.nodes()]
-# edge_labels = dict([((u, v,), d['weight'])
-#                  for u, v, d in G.edges(data=True)])
-# red_edges = [('tramwaj', 'D'), ('D', 'A')]
-# edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
-#
-# pos = nx.spring_layout(Robot)
-# pos = graphviz_layout(Robot, prog='dot')
-# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-nx.draw(Robot, node_size=4000, edge_cmap=plt.cm.Reds, with_labels=True, edge_color='black', node_color='blue')
-pylab.show()
-nx.draw(Przybijanie, node_size=4000, edge_cmap=plt.cm.Reds, with_labels=True, edge_color='black', node_color='green')
-pylab.show()
-nx.draw(Ladowanie, node_size=4000, edge_cmap=plt.cm.Reds, with_labels=True, edge_color='black', node_color='red')
-pylab.show()
+Robot.add_edges_from([(s1, s2),(s1,s1), (s2, s1), (s2, s3), (s3, s2), (s3, s4), (s4,s3), (s3, s5), (s5,s3),(s3,s3)])
+Przybijanie.add_edges_from([(s5,s6),(s6,s5),(s6,s7),(s6,s8),(s7,s9),(s9,s7),(s9,s9),(s10,s10),(s8,s10),(s10,s8),(s7,s11),(s8,s11),(s11,s12),(s12,s6)])
+Ladowanie.add_edges_from([(s4,s13),(s13,s4),(s13,s15),(s15,s13),(s15,s15),(s13,s14),(s14,s13)])
+Robot.add_edges_from(Przybijanie.edges)
+Robot.add_edges_from(Ladowanie.edges)
+
+blockades = False
+list = list(Robot)
+
+for i in range(len(list)):
+    for j in range(len(list)):
+        source = list[i]
+        target = list[j]
+        is_path = nx.has_path(Robot, source=source, target=target)
+        # print(is_path, source, target)
+        source.replace('\n', ' ')
+        target.replace('\n', ' ')
+        if not is_path:
+            print("Jest blokada miedzy " + source + " a " + target)
+            blockades = True
+            break
+if not blockades:
+    print("Brak blokad")
+
+def show_path(source, target):
+    for i in range(len(list)):
+        for j in range(len(list)):
+            is_path = nx.has_path(Robot, source=source, target=target)
+            path = nx.shortest_path(Robot, source=source, target=target)
+            if not is_path:
+                print("NO PATH")
+                break
+    for k in range(len(path)):
+        path[k] = path[k].replace('\n', ' ')
+    print(path)
+
+show_path(s7,s8)
+
+Robot.graph['edge'] = {'arrowsize': '0.6', 'splines': 'curved'}
+Robot.graph['graph'] = {'scale': '4'}
+Graf = to_agraph(Robot)
+Graf.layout('dot')
+Graf.draw('Graf.png')
+img = mpimg.imread('Graf.png')
+imgplot = plt.show(img)
+# plt.axis('off')
+# plt.show(imgplot)
